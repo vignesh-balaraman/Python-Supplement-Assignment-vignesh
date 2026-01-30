3f46bc86f632bd930e7b68b8cdacebb1abd0ab02
vigneshvicky <suriyavignesh3306@gmail.com>
2026-01-30 16:09:35 +0530

    changed python

--- problem_22.py
+++ problem_22.py
@@ -2,7 +2,7 @@
 # Find and fix the error
 
 nested_list = [[1, 2], [3, 4], [5, 6]]
-flat_list = []
+flat_list = [] 
 for sublist in nested_list:
     for item in sublist:
         flat_list.append(item)
