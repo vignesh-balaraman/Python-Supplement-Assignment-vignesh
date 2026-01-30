ccdd7f3bfb71902c7dd38e00f6e703fb48fd8516
vigneshvicky <suriyavignesh3306@gmail.com>
2026-01-30 16:08:18 +0530

    changed python_21

--- problem_21.py
+++ problem_21.py
@@ -5,7 +5,7 @@ def is_sorted(lst):
     for i in range(len(lst) - 1):
         if lst[i] > lst[i + 1]:
             return False
-    return True
+    return True 
 
 numbers = [1, 2, 3, 4, 5]
 print(f"Is sorted: {is_sorted(numbers)}")
