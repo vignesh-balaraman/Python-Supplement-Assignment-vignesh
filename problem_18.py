d42fba8a8bf674208585f714f3a35c838926b6f6
vigneshvicky <suriyavignesh3306@gmail.com>
2026-01-30 16:03:09 +0530

    changed python_18

--- problem_18.py
+++ problem_18.py
@@ -1,6 +1,6 @@
 # Problem 18: Count words in a sentence
 # Find and fix the error
 
-sentence = "Python is a great programming language"
+sentence =  "Python is a great programming language"
 word_count = len(sentence.split())
 print(f"Number of words: {word_count}")
