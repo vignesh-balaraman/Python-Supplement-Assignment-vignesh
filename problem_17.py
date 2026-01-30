f581eb83df5158297a6c260f77b3504281e300cd
vigneshvicky <suriyavignesh3306@gmail.com>
2026-01-30 16:01:42 +0530

    changed python_17

--- problem_17.py
+++ problem_17.py
@@ -8,5 +8,5 @@ def capitalize_words(text):
         capitalized.append(word.capitalize())
     return " ".join(capitalized)
 
-sentence = "hello world from python"
+sentence ="hello world from python"
 print(capitalize_words(sentence))
